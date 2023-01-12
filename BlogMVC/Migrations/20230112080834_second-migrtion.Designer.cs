62028d7cfab660b7f9ce89110c4d1cdaa52fd4c5
Nikola Valkov <bulgarianmapper64@gmail.com>
2023-01-12 10:09:01 +0200

    second migration done allowing nulls

--- BlogMVC/Migrations/20230112080834_second-migrtion.Designer.cs
+++ BlogMVC/Migrations/20230112080834_second-migrtion.Designer.cs
@@ -4,6 +4,7 @@ using BlogMVC.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlogMVC.Migrations
 {
     [DbContext(typeof(BlogContext))]
-    partial class BlogContextModelSnapshot : ModelSnapshot
+    [Migration("20230112080834_second-migrtion")]
+    partial class secondmigrtion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- BlogMVC/Models/Blog.cs
+++ BlogMVC/Models/Blog.cs
@@ -6,8 +6,8 @@ namespace BlogMVC.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime? CreatedDate { get; set; }
     }
 }
