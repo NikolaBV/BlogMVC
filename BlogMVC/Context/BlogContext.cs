d5b4910343e2a897bc593c38b0085c3b7365270c
Nikola Valkov <bulgarianmapper64@gmail.com>
2023-01-12 09:34:06 +0200

    Add project files.

---
+++ BlogMVC/Context/BlogContext.cs
@@ -0,0 +1,14 @@
+﻿using BlogMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogMVC.Context
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext(DbContextOptions options) :base(options)
+        {
+
+        }
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
---
+++ BlogMVC/Models/Blog.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogMVC.Models
+{
+    public class Blog
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
